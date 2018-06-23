ae5d2b7ecdae24ed541458b2e82a8de073a27b4c
Krzysztof Wawer <krzysztof.wawer@gmail.com>
2018-06-23 22:41:06 +0200

    save issue independent for comicbookdb

--- lib/caching/comic_book_db/single.rb
+++ lib/caching/comic_book_db/single.rb
@@ -17,5 +17,10 @@ class Caching::ComicBookDB::Single
     result = parse_results(response)
     path = "comicbookdb/series/#{caching.id_partition(result["cdb_id"])}.json"
     caching.save(path, result)
+
+    result["issues"].each do |issue|
+      path = "comicbookdb/issues/#{caching.id_partition(issue["cdb_id"])}_brief.json"
+      caching.save(path, issue)
+    end
   end
 end
